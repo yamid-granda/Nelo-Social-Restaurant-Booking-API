2b7989d44ffa7315830bd97db57fde374720b0cc
yamid-granda <vallenatomanizales@gmial.com>
2024-07-08 07:43:02 -0500

    feat: restaurants search by datetime show available tables

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Restaurant
 from tables.serializers.table_item_serializer import TableItemSerializer
-from restaurants_diets.serializers.restaurant_diet_item_serializer import (
-    RestaurantDietItemSerializer,
-)
+from tables.models import Table
+from django.db.models import Q
+from reservations.utils import get_limits_from_str_date
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -17,8 +17,27 @@ class RestaurantSerializer(serializers.ModelSerializer):
 
 
 class RestaurantDetailsSerializer(serializers.ModelSerializer):
-    diets = RestaurantDietItemSerializer(many=True, read_only=True)
     tables = TableItemSerializer(many=True, read_only=True)
+
+    def to_representation(self, obj):
+        query_params = self.context["request"].query_params
+        datetime_param: str = query_params.get("datetime")
+        datetime: str | None = datetime_param if datetime_param else None
+        data = super().to_representation(obj)
+
+        if datetime:
+            min_datetime, max_datetime = get_limits_from_str_date(datetime)
+
+            tables = Table.objects.filter(
+                Q(restaurant_id=data["id"])
+                & ~Q(
+                    reservations__datetime__gt=min_datetime,
+                    reservations__datetime__lt=max_datetime,
+                )
+            )
+            data["tables"] = TableItemSerializer(tables, many=True).data
+
+        return data
 
     class Meta:
         model = Restaurant
@@ -26,6 +45,5 @@ class RestaurantDetailsSerializer(serializers.ModelSerializer):
             "id",
             "name",
             "created_at",
-            "diets",
             "tables",
         )
--- tables/serializers/table_item_serializer.py
+++ tables/serializers/table_item_serializer.py
@@ -1,19 +1,13 @@
 from rest_framework import serializers
 from ..models import Table
-
-from reservations.serializers.reservation_item_serializer import (
-    ReservationItemSerializer,
-)
+from tables.models import Table
 
 
 class TableItemSerializer(serializers.ModelSerializer):
-    reservations = ReservationItemSerializer(many=True, read_only=True)
-
     class Meta:
         model = Table
         fields = (
             "id",
             "name",
             "capacity",
-            "reservations",
         )
